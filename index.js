5f0b50ced1ea736c228ea5a325c7ff1f795a7011
AnoopHLIS <anoop.hyperlinkinfosystempvtltd@gmail.com>
2023-04-21 10:36:46 +0530

    Task 4

--- index.js
+++ index.js
@@ -14,35 +14,35 @@ var paidTickets = new Map();
  * Function to load initials on refresh page
  */
 function loadInitial() {
-  if (!localStorage.getItem("tickets")) {
-    localStorage.setItem("tickets", JSON.stringify(tickets));
-  } else {
-    tickets = JSON.parse(localStorage.getItem("tickets"));
-  }
+    if (!localStorage.getItem("tickets")) {
+        localStorage.setItem("tickets", JSON.stringify(tickets));
+    } else {
+        tickets = JSON.parse(localStorage.getItem("tickets"));
+    }
 
-  if (!localStorage.getItem("paidTickets")) {
-    localStorage.setItem("paidTickets", JSON.stringify(paidTickets));
-  } else {
-    paidTickets = JSON.parse(localStorage.getItem("paidTickets"));
-  }
+    if (!localStorage.getItem("paidTickets")) {
+        localStorage.setItem("paidTickets", JSON.stringify(paidTickets));
+    } else {
+        paidTickets = JSON.parse(localStorage.getItem("paidTickets"));
+    }
 
-  if (!localStorage.getItem("barcodes")) {
-    localStorage.setItem("bardodes", barcodes);
-  } else {
-    barcodes = localStorage.getItem("barcodes");
-  }
+    if (!localStorage.getItem("barcodes")) {
+        localStorage.setItem("bardodes", barcodes);
+    } else {
+        barcodes = localStorage.getItem("barcodes");
+    }
 
-  if (!localStorage.getItem("ticketLimit")) {
-    localStorage.setItem("ticketLimit", ticketLimit);
-  } else {
-    ticketLimit = localStorage.getItem("ticketLimit");
-  }
+    if (!localStorage.getItem("ticketLimit")) {
+        localStorage.setItem("ticketLimit", ticketLimit);
+    } else {
+        ticketLimit = localStorage.getItem("ticketLimit");
+    }
 
-  if (!localStorage.getItem("ticketCurrentCount")) {
-    localStorage.setItem("ticketCurrentCount", ticketCurrentCount);
-  } else {
-    ticketCurrentCount = localStorage.getItem("ticketCurrentCount");
-  }
+    if (!localStorage.getItem("ticketCurrentCount")) {
+        localStorage.setItem("ticketCurrentCount", ticketCurrentCount);
+    } else {
+        ticketCurrentCount = localStorage.getItem("ticketCurrentCount");
+    }
 }
 
 /**
@@ -50,21 +50,21 @@ function loadInitial() {
  * @returns new ticket
  */
 function getTicket() {
-  if (ticketCurrentCount == ticketLimit) {
-    alert("No space available for new consumer");
-    return false;
-  }
-  var barcodeForTicket = generateTicketBarcode(16);
-  tickets[barcodeForTicket] = JSON.stringify({
-    id: ticketCurrentCount,
-    barcode: barcodeForTicket,
-    date: new Date(),
-  });
-  barcodes[ticketCurrentCount] = barcodeForTicket;
-  ticketCurrentCount++;
-  localStorage.setItem("tickets", JSON.stringify(tickets));
-  localStorage.setItem("ticketCurrentCount", ticketCurrentCount);
-  localStorage.setItem("barcodes", barcodes);
+    if (ticketCurrentCount === ticketLimit) {
+        alert("No space available for new consumer");
+        return false;
+    }
+    var barcodeForTicket = generateTicketBarcode(16);
+    tickets[barcodeForTicket] = JSON.stringify({
+        id: ticketCurrentCount,
+        barcode: barcodeForTicket,
+        date: new Date(),
+    });
+    barcodes[ticketCurrentCount] = barcodeForTicket;
+    ticketCurrentCount++;
+    localStorage.setItem("tickets", JSON.stringify(tickets));
+    localStorage.setItem("ticketCurrentCount", ticketCurrentCount);
+    localStorage.setItem("barcodes", barcodes);
 }
 
 /**
@@ -73,7 +73,7 @@ function getTicket() {
  * @returns
  */
 function getTicketByBarcode(barcode) {
-  return tickets[barcode];
+    return tickets[barcode];
 }
 
 /**
@@ -82,15 +82,15 @@ function getTicketByBarcode(barcode) {
  * @returns
  */
 function generateTicketBarcode(length) {
-  let result = "";
-  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  const charactersLength = characters.length;
-  let counter = 0;
-  while (counter < length) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    counter += 1;
-  }
-  return result;
+    let result = "";
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        counter += 1;
+    }
+    return result;
 }
 
 /**
@@ -98,62 +98,114 @@ function generateTicketBarcode(length) {
  * @param barcode
  */
 function calculatePrice(barcode) {
-  var paidTicket = paidTickets[barcode];
+    var paidTicket = paidTickets[barcode];
 
-  if (paidTicket) {
-    var paidTicketJson = JSON.parse(paidTicket);
-    return (
-      "Price: 0 ;" +
-      "Amount Paid: " +
-      paidTicketJson.amount +
-      " ; Payment Method: " +
-      paymentMethod[paidTicketJson.paymentMethod] +
-      " ; Paid at: " +
-      new Date(paidTicketJson.paidAt)
-    );
-  }
-  var ticket = tickets[barcode];
-  var current = new Date();
-  var ticketTime = new Date(JSON.parse(ticket).date);
-  var timeDifferenceInHrs =
-    Math.abs(current.getTime() - ticketTime.getTime()) / 36e5;
+    if (paidTicket) {
+        var paidTicketJson = JSON.parse(paidTicket);
 
-  if (timeDifferenceInHrs < minimumTime || timeDifferenceInHrs == minimumTime) {
-    return timePerHr;
-  } else {
-    var timeDiff = timeDifferenceInHrs;
-    // setting timePerHr
-    var price = timePerHr;
-    // deducting minimum 3 hrs
-    timeDiff = timeDiff - minimumTime;
+        var currentTime = new Date().getTime();
+        var paidTime = new Date(paidTicketJson.paidAt).getTime();
 
-    // calculating price based on remaining hrs * minimumTime
-    price = price + Math.floor(timeDiff) * extendTimePerHr;
-    return price;
-  }
+        var diffInMinutes = (currentTime - paidTime) / 6e4;
+
+        // if took more than 15 mins before exit, then need to make another payment
+        if (diffInMinutes > 15) {
+            var diffInHr = Math.floor(diffInMinutes / 60);
+            var extraPrice;
+            if (diffInHr === 0) {
+                extraPrice = extendTimePerHr;
+            } else {
+                extraPrice = extendTimePerHr * diffInHr;
+            }
+
+            return extraPrice;
+        }
+
+
+        return paidTicketJson;
+    }
+    var ticket = tickets[barcode];
+    var current = new Date();
+    var ticketTime = new Date(JSON.parse(ticket).date);
+    var timeDifferenceInHrs =
+        Math.abs(current.getTime() - ticketTime.getTime()) / 36e5;
+
+    if (timeDifferenceInHrs < minimumTime || timeDifferenceInHrs === minimumTime) {
+        return timePerHr;
+    } else {
+        var timeDiff = timeDifferenceInHrs;
+        // setting timePerHr
+        var price = timePerHr;
+        // deducting minimum 3 hrs
+        timeDiff = timeDiff - minimumTime;
+
+        // calculating price based on remaining hrs * minimumTime
+        price = price + Math.floor(timeDiff) * extendTimePerHr;
+        return price;
+    }
 }
 
 function payTicket(barcode, paymentMethodCode) {
-  // check if already paid or not
-  var paidTicket = paidTickets[barcode];
+    // check if already paid or not
+    var paidTicket = paidTickets[barcode];
 
-  if (paidTicket) {
-    var paidTicketJson = JSON.parse(paidTicket);
-    return (
-      "Price: 0 ;" +
-      "Amount Paid: " +
-      paidTicketJson.amount +
-      " ; Payment Method: " +
-      paymentMethod[paidTicketJson.paymentMethod] +
-      " ; Paid at: " +
-      new Date(paidTicketJson.paidAt)
-    );
-  } else {
-    paidTickets[barcode] = JSON.stringify({
-      amount: calculatePrice(barcode),
-      paymentMethod: paymentMethodCode,
-      paidAt: new Date(),
-    });
-    localStorage.setItem("paidTickets", JSON.stringify(paidTickets));
-  }
+    if (paidTicket) {
+        var paidTicketJson = JSON.parse(paidTicket);
+
+        var currentTime = new Date().getTime();
+        var paidTime = new Date(paidTicketJson.paidAt).getTime();
+
+        var diffInMinutes = (currentTime - paidTime) / 6e4;
+
+        // if took more than 15 mins before exit, then need to make another payment
+        if (diffInMinutes > 15) {
+            var diffInHr = Math.floor(diffInMinutes / 60);
+            var extraPrice;
+            if (diffInHr === 0) {
+                extraPrice = extendTimePerHr;
+            } else {
+                extraPrice = extendTimePerHr * diffInHr;
+            }
+            paidTickets[barcode] = JSON.stringify({
+                amount: calculatePrice(barcode),
+                paymentMethod: paymentMethodCode,
+                paidAt: new Date(),
+                extraAmount: extraPrice,
+                ticketPaidAt: new Date(paidTicketJson.paidAt)
+            });
+            localStorage.setItem("paidTickets", JSON.stringify(paidTickets));
+        } else {
+
+            // if already paid and ready for exit
+            return paidTicketJson;
+        }
+    } else {
+        paidTickets[barcode] = JSON.stringify({
+            amount: calculatePrice(barcode),
+            paymentMethod: paymentMethodCode,
+            paidAt: new Date(),
+        });
+        // ticketCurrentCount--; // to be uncomment when need to exit
+        // localStorage.setItem('ticketCurrentCount', ticketCurrentCount);
+        localStorage.setItem("paidTickets", JSON.stringify(paidTickets));
+    }
+}
+
+function getTicketState(barcode) {
+    var paidTicket = paidTickets[barcode];
+    if (paidTicket) {
+        var paidTicketJson = JSON.parse(paidTicket);
+
+        var currentTime = new Date().getTime();
+        var paidTime = new Date(paidTicketJson.paidAt).getTime();
+
+        var diffInMinutes = (currentTime - paidTime) / 6e4;
+
+        if (diffInMinutes > 15)
+            return "unpaid";
+        return "paid";
+
+
+    }
+    return "unpaid";
 }
